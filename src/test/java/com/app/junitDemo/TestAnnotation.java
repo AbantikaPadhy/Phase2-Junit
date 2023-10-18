c3ad1ab4414052e91a7ca6e33f96be98f92110d4
Abantika <abantikapadhy06@gmail.com>
2023-10-18 09:21:17 +0530

    done

---
+++ src/test/java/com/app/junitDemo/TestAnnotation.java
@@ -0,0 +1,10 @@
+package com.app.junitDemo;
+import org.junit.jupiter.api.Test;
+public class TestAnnotation {
+
+	@Test  // execute the below test
+	public void Method1()  // unit Test method
+	{
+		System.out.println("Hello Junit");
+	}
+}
